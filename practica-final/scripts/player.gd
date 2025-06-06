7542f29f05a7b0853bfd61510bc2b201f9a315dd
Max <u1995381@campus.udg.edu>
2025-06-06 14:11:21 +0200

    Afegit sprite del primer enemic

--- practica-final/scripts/player.gd
+++ practica-final/scripts/player.gd
@@ -1,25 +1,31 @@
 extends CharacterBody2D
-
+@onready var sprites = $AnimatedSprite2D;
 
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
+#const GRAVITY = 981
 
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
-
+		if velocity.y > 0:
+			sprites.play("jump")
+		elif velocity.y < 0:
+			sprites.play("fall")
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	elif Input.is_action_just_pressed("ui_salt"):
 		velocity.y = JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
+	var direction := Input.get_axis("ui_esq", "ui_dret")
 	if direction:
 		velocity.x = direction * SPEED
+		sprites.flip_h = direction < 0
+		sprites.play("run");
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
-
+		sprites.play("idle")
 	move_and_slide()
