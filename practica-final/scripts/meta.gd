d4ca75bf14e66195ec3768f23c009628616a4368
Max <u1995381@campus.udg.edu>
2025-06-14 16:38:34 +0200

    Afegida lògica de canvi de nivell

--- practica-final/scripts/meta.gd
+++ practica-final/scripts/meta.gd
@@ -1,6 +1,7 @@
 extends RigidBody2D
 # @export var instrument: Sprite2D
 var victoria = load("res://escenas/menu_victoria.tscn")
+var nivell
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass
@@ -13,4 +14,9 @@ func _process(delta: float) -> void:
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
 	if body is player:
-		get_tree().change_scene_to_packed(victoria)
+		if nivell == 1:
+			get_tree().change_scene_to_file("res://escenas/level_2.tscn")
+		elif nivell == 2:
+			pass # passarem al 3 més endavant
+		else:
+			get_tree().change_scene_to_packed(victoria)
