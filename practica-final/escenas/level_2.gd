60aaea58eff906401411dc208e1ea82135b6da75
Max <u1995381@campus.udg.edu>
2025-06-16 16:05:50 +0200

    Enemics nivell 2 es mouen

--- practica-final/escenas/level_2.gd
+++ practica-final/escenas/level_2.gd
@@ -1,5 +1,10 @@
 extends Node2D
-
+@onready var path_follow_1 : PathFollow2D = $Path2D/PathFollow2D
+@onready var path_follow_2 : PathFollow2D = $Path2D2/PathFollow2D
+@onready var path_follow_3 : PathFollow2D = $Path2D3/PathFollow2D
+@onready var dimoni_1 = $Path2D/PathFollow2D/enemic
+@onready var dimoni_2 = $Path2D2/PathFollow2D/enemic
+@onready var fantasma_1 = $Path2D3/PathFollow2D/Fantasma
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	$Meta.nivell = 3
@@ -7,4 +12,22 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	#Tots aquests if son per a evitar intentar moure enemics morts, cosa que crashea el joc
+	if is_instance_valid(dimoni_1):
+		path_follow_1.progress += dimoni_1.velocitat * delta
+	if is_instance_valid(dimoni_2):
+		path_follow_2.progress += dimoni_2.velocitat * delta
+	if is_instance_valid(fantasma_1):
+		path_follow_3.progress += fantasma_1.velocitat * delta
+
+#Ho fem aqui ja que aixi en cada nivell podem modificar els atacs del enemic
+
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	if body is player:
+		body.morir()
+
+func _on_atac_timeout() -> void:
+	dimoni_1.disparar($player)
+
+func _on_atac_2_timeout() -> void:
+	dimoni_2.disparar($player)
