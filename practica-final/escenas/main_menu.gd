460ddd772bf72bb188327c352c9425183414f2b9
BarbaNesquick <gorka@alexsancho.name>
2025-06-19 16:44:39 +0200

    AASS

--- practica-final/escenas/main_menu.gd
+++ practica-final/escenas/main_menu.gd
@@ -10,9 +10,5 @@ func _on_boto_sortir_pressed() -> void:
 	get_tree().quit()
 
 
-func _on_boto_opcions_pressed() -> void:
-	pass # Replace with function body.
-
-
 func _on_boto_jugar_pressed() -> void:
 	get_tree().change_scene_to_packed(nivell)
